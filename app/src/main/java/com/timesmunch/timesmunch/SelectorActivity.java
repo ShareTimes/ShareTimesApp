6571085da1b27d701227a7a79b599c6466fc8483
Faraz Fazli <farazfazli@gmail.com>
2016-03-08 22:30:59 -0500

    Grant ability to insert image into ListView using Picasso

--- app/src/main/java/com/timesmunch/timesmunch/SelectorActivity.java
+++ app/src/main/java/com/timesmunch/timesmunch/SelectorActivity.java
@@ -5,12 +5,15 @@ import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v4.widget.CursorAdapter;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
@@ -46,7 +49,25 @@ public class SelectorActivity extends AppCompatActivity {
                 TextView selectionTitle = (TextView) view.findViewById(R.id.selectionTitle);
                 ImageView imageView = (ImageView) view.findViewById(R.id.imageView);
                 String title = cursor.getString(cursor.getColumnIndex(NewsWireDBHelper.COLUMN_ARTICLE_TITLE));
+                String url = cursor.getString(cursor.getColumnIndex(NewsWireDBHelper.COLUMN_IMAGE_URL));
+                // Line above is grabbing the URL from the database.
+                Log.i("[URL]", url);
                 selectionTitle.setText(title);
+
+
+                Picasso.with(context).load(url).into(imageView);
+                /**
+                 * Above is the really cool line which handles loading this image into the ImageView.
+                 * There are many things you can do like .centerCrop.into(imageView); or something
+                 * like .centerInside().into(imageView); experiment and see which looks best.
+                 */
+
+                /**
+                 * Hope this makes sense to you, please
+                 * text me if you have any questions!
+                 * Wish me luck in San Francisco. See
+                 * you on Monday.
+                 */
             }
         };
 
